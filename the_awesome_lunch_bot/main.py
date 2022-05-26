fd3921f1a57cb8ce5f71cb15e90873c4baeb4b6c
KaOs1 <davellidavide@gmail.com>
2022-05-26 21:45:13 +0200

    fix log file path

--- the_awesome_lunch_bot/main.py
+++ the_awesome_lunch_bot/main.py
@@ -1,10 +1,13 @@
-from distutils.log import INFO
+from distutils.debug import DEBUG
 from skype_handler import SkypeHandler
+from decouple import config
 
 import logging
 
+LOG_FILE = config("DATA_FOLDER") + "/lunch_bot.log"
+
 if __name__ == "__main__":
-    logging.basicConfig(filename="./data/lunch_bot.log", level=INFO)
+    logging.basicConfig(filename=LOG_FILE, level=DEBUG)
     print("Starting application")
     skype_handler = SkypeHandler()
     skype_handler.loop()
